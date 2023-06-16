3cd3d16ce2c0e11ab5e9efad2d035752233b7495
celesdream <josesiyo@steve>
2023-06-15 21:34:59 -0600

    movies

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:movies/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget 
+{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Peliculas en cartelera"),
+        elevation: 0,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search_outlined))
+        ],
+
+      ),
+      body: Column(
+        children: [
+
+          CardSwiper()
+        ],
+      )
+    );
+  }
+}
